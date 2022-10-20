b58f96f9e3e969dbc0a1294d9a3c2bc236677cc8
smart1206 <proharvester@outlook.com>
2022-10-19 19:53:04 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# RESTful API Node Server
+A project for building RESTful APIs using Node.js, Express.
+
+## Manual Installation
+Clone the repo:
+
+```bash
+git clone git@github.com:smart1206/react-redux-test-fe.git
+cd react-redux-test-fe
+```
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+## Commands
+
+Running locally:
+
+```bash
+npm start
+```
---
+++ app.js
@@ -0,0 +1,74 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import fetch from 'node-fetch'
+
+const app = express();
+
+// parse urlencoded request body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse json request body
+app.use(bodyParser.json());
+
+// enable cors
+app.use(cors({
+  origin: "*",
+  methods: "post",
+  optionsSuccessStatus: 200
+}));
+
+const port = 5000;
+
+app.listen(port, (error) => {
+  if (error) { //handle error
+    throw new Error(error);
+  }
+  console.log("Backend is running", port);
+});
+
+/*
+  return back all the user information
+*/
+app.get("/getUserInfos", async (req, res) => {
+  try {
+    const resp = await fetch(
+      `http://jsonplaceholder.typicode.com/posts`,
+      {
+        method: 'GET'
+      }
+    );
+    const data = await resp.json();
+
+    // return response
+    res.status(200).send({ success: true, result: data });
+  } catch (err) { // error handling
+    res.status(200).send({ success: false });
+    console.log(err)
+    return;
+  }
+})
+
+/*
+  return back one user information by id
+*/
+app.get("/getUserInfo", async (req, res) => {
+  try {
+    const resp = await fetch(
+      `http://jsonplaceholder.typicode.com/posts`,
+      {
+        method: 'GET'
+      }
+    );
+    const data = await resp.json();
+
+    const filter = data.find(elem => elem.id == req.query["id"]);
+
+    // return response
+    res.status(200).send({ success: true, result: filter });
+  } catch (err) { // error handling
+    res.status(200).send({ success: false });
+    console.log(err)
+    return;
+  }
+})
